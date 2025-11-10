3e356d432e1cad20a6cac23da0751f3d6976a939
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-11-10 19:23:04 +0500

    feat: добавить правила для импортов в eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	images: {
+		unoptimized: true,
+	},
+	rules: {
+		'sort-imports': [
+			'error',
+			{
+				ignoreCase: false,
+				ignoreDeclarationSort: true,
+				ignoreMemberSort: false,
+				memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+				allowSeparatedGroups: true,
+			},
+		],
+	},
+};
 
-export default nextConfig
+export default nextConfig;
