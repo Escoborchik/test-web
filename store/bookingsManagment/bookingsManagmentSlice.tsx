0f6d817b2808ca979b0c38f19bd738e133a68a10
Escoborchik <borovinskiy_2004@mail.ru>
2025-11-18 10:26:39 +0500

    refactor: мелкие правки1.1

--- store/bookingsManagment/bookingsManagmentSlice.tsx
+++ store/bookingsManagment/bookingsManagmentSlice.tsx
@@ -1,171 +1,171 @@
-import { Booking, BookingStatus } from '@/types/booking';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Booking, BookingStatus } from "@/types/booking";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface BookingManagmentState {
-	bookings: Booking[];
+  bookings: Booking[];
 }
 
 const initialState: BookingManagmentState = {
-	bookings: [
-		{
-			id: 'booking-1',
-			firstName: 'Алексей',
-			lastName: 'Иванов',
-			phone: '+7 (900) 123-45-67',
-			email: 'alexey.ivanov@example.com',
-			courtId: 'court-1',
-			date: ['2025-11-22'],
-			time: '09:00-11:00',
-			duration: 2,
-			price: 3600,
-			status: 'confirmed',
-			isRecurring: false,
-			tariffId: 'tariff-single-visit',
-			extras: [{ extraId: 'extra-racket-rental', quantity: 1 }],
-		},
-		{
-			id: 'booking-4',
-			firstName: 'Анатолий',
-			lastName: 'Лужанин',
-			phone: '+7 (900) 456-78-91',
-			email: 'anatoly.luzhanin@example.com',
-			courtId: 'court-1',
-			date: ['2025-11-22'],
-			time: '11:00-14:00',
-			duration: 3,
-			price: 5400,
-			status: 'pending',
-			isRecurring: false,
-			tariffId: 'tariff-single-visit',
-			extras: [],
-		},
-		{
-			id: 'booking-5',
-			firstName: 'Дмитрий',
-			lastName: 'Махнев',
-			phone: '+7 (916) 888-77-66',
-			email: 'dmitry.m@example.com',
-			courtId: 'court-3',
-			date: [
-				'2025-11-12',
-				'2025-11-15',
-				'2025-11-19',
-				'2025-11-22',
-				'2025-11-26',
-				'2025-11-29',
-			],
-			time: '14:00-15:00',
-			duration: 1,
-			price: 1700,
-			status: 'confirmed',
-			isRecurring: true,
-			tariffId: 'tariff-subscription',
-			extras: [{ extraId: 'extra-ball-set', quantity: 2 }],
-			recurringDetails: {
-				startDate: '2025-11-12',
-				endDate: '2025-11-29',
-				weeks: 3,
-				days: ['Wed', 'Sat'],
-			},
-		},
-		{
-			id: 'booking-2',
-			firstName: 'Мария',
-			lastName: 'Соколова',
-			phone: '+7 (921) 555-66-77',
-			email: 'maria.sokolova@example.com',
-			courtId: 'court-2',
-			date: ['2025-11-23'],
-			time: '18:00-20:00',
-			duration: 2,
-			price: 3600,
-			status: 'pending',
-			isRecurring: false,
-			tariffId: 'tariff-single-visit',
-			extras: [],
-		},
-		{
-			id: 'booking-3',
-			firstName: 'Дмитрий',
-			lastName: 'Кузнецов',
-			phone: '+7 (916) 777-88-99',
-			email: 'dmitry.k@example.com',
-			courtId: 'court-3',
-			date: ['2025-11-24', '2025-11-26', '2025-11-28'],
-			time: '08:00-09:00',
-			duration: 1,
-			price: 1800,
-			status: 'confirmed',
-			isRecurring: true,
-			tariffId: 'tariff-many-visit',
-			extras: [],
-			recurringDetails: {
-				startDate: '2025-11-24',
-				endDate: '2025-11-28',
-				weeks: 4,
-				days: ['Mon', 'Wed', 'Fri'],
-			},
-		},
-		{
-			id: 'booking-6',
-			firstName: 'Матвей',
-			lastName: 'Гусаров',
-			phone: '+7 (915) 111-77-88',
-			email: 'matvey.g@example.com',
-			courtId: 'court-3',
-			date: ['2025-11-25'],
-			time: '08:00-09:00',
-			duration: 2,
-			price: 3600,
-			status: 'pending-payment',
-			tariffId: 'tariff-single-visit',
-			isRecurring: false,
-			extras: [],
-		},
-	],
+  bookings: [
+    {
+      id: "booking-1",
+      firstName: "Алексей",
+      lastName: "Иванов",
+      phone: "+7 (900) 123-45-67",
+      email: "alexey.ivanov@example.com",
+      courtId: "court-1",
+      date: ["2025-11-22"],
+      time: "09:00-11:00",
+      duration: 2,
+      price: 3600,
+      status: "confirmed",
+      isRecurring: false,
+      tariffId: "tariff-single-visit",
+      extras: [{ extraId: "extra-racket-rental", quantity: 1 }],
+    },
+    {
+      id: "booking-4",
+      firstName: "Анатолий",
+      lastName: "Лужанин",
+      phone: "+7 (900) 456-78-91",
+      email: "anatoly.luzhanin@example.com",
+      courtId: "court-1",
+      date: ["2025-11-22"],
+      time: "11:00-14:00",
+      duration: 3,
+      price: 5400,
+      status: "pending",
+      isRecurring: false,
+      tariffId: "tariff-single-visit",
+      extras: [],
+    },
+    {
+      id: "booking-5",
+      firstName: "Дмитрий",
+      lastName: "Махнев",
+      phone: "+7 (916) 888-77-66",
+      email: "dmitry.m@example.com",
+      courtId: "court-3",
+      date: [
+        "2025-11-12",
+        "2025-11-15",
+        "2025-11-19",
+        "2025-11-22",
+        "2025-11-26",
+        "2025-11-29",
+      ],
+      time: "14:00-15:00",
+      duration: 1,
+      price: 1700,
+      status: "confirmed",
+      isRecurring: true,
+      tariffId: "tariff-subscription",
+      extras: [{ extraId: "extra-ball-set", quantity: 2 }],
+      recurringDetails: {
+        startDate: "2025-11-12",
+        endDate: "2025-12-03",
+        weeks: 4,
+        days: ["Wed", "Sat"],
+      },
+    },
+    {
+      id: "booking-2",
+      firstName: "Мария",
+      lastName: "Соколова",
+      phone: "+7 (921) 555-66-77",
+      email: "maria.sokolova@example.com",
+      courtId: "court-2",
+      date: ["2025-11-23"],
+      time: "18:00-20:00",
+      duration: 2,
+      price: 3600,
+      status: "pending",
+      isRecurring: false,
+      tariffId: "tariff-single-visit",
+      extras: [],
+    },
+    {
+      id: "booking-3",
+      firstName: "Дмитрий",
+      lastName: "Кузнецов",
+      phone: "+7 (916) 777-88-99",
+      email: "dmitry.k@example.com",
+      courtId: "court-3",
+      date: ["2025-11-24", "2025-11-26", "2025-11-28"],
+      time: "08:00-09:00",
+      duration: 1,
+      price: 1800,
+      status: "confirmed",
+      isRecurring: true,
+      tariffId: "tariff-many-visit",
+      extras: [],
+      recurringDetails: {
+        startDate: "2025-11-24",
+        endDate: "2025-11-28",
+        weeks: 4,
+        days: ["Mon", "Wed", "Fri"],
+      },
+    },
+    {
+      id: "booking-6",
+      firstName: "Матвей",
+      lastName: "Гусаров",
+      phone: "+7 (915) 111-77-88",
+      email: "matvey.g@example.com",
+      courtId: "court-3",
+      date: ["2025-11-25"],
+      time: "08:00-09:00",
+      duration: 2,
+      price: 3600,
+      status: "pending-payment",
+      tariffId: "tariff-single-visit",
+      isRecurring: false,
+      extras: [],
+    },
+  ],
 };
 
 const bookingsManagmentSlice = createSlice({
-	name: 'bookingsManagment',
-	initialState,
-	reducers: {
-		createBooking: (state, action: PayloadAction<Booking>) => {
-			state.bookings.unshift(action.payload);
-		},
-		updateStatusBooking: (
-			state,
-			action: PayloadAction<{
-				id: string;
-				status: BookingStatus;
-			}>
-		) => {
-			const rightBooking = state.bookings.find(
-				(booking) => booking.id === action.payload.id
-			);
+  name: "bookingsManagment",
+  initialState,
+  reducers: {
+    createBooking: (state, action: PayloadAction<Booking>) => {
+      state.bookings.unshift(action.payload);
+    },
+    updateStatusBooking: (
+      state,
+      action: PayloadAction<{
+        id: string;
+        status: BookingStatus;
+      }>
+    ) => {
+      const rightBooking = state.bookings.find(
+        (booking) => booking.id === action.payload.id
+      );
 
-			if (rightBooking) rightBooking.status = action.payload.status;
-		},
-		deleteDateFromRecurringBooking: (
-			state,
-			action: PayloadAction<{ id: string; date: string }>
-		) => {
-			const rightBooking = state.bookings.find(
-				(booking) => booking.id === action.payload.id
-			);
+      if (rightBooking) rightBooking.status = action.payload.status;
+    },
+    deleteDateFromRecurringBooking: (
+      state,
+      action: PayloadAction<{ id: string; date: string }>
+    ) => {
+      const rightBooking = state.bookings.find(
+        (booking) => booking.id === action.payload.id
+      );
 
-			if (rightBooking?.date) {
-				const index = rightBooking.date.indexOf(action.payload.date);
+      if (rightBooking?.date) {
+        const index = rightBooking.date.indexOf(action.payload.date);
 
-				if (index !== -1) rightBooking.date.splice(index, 1);
-			}
-		},
-	},
+        if (index !== -1) rightBooking.date.splice(index, 1);
+      }
+    },
+  },
 });
 
 export const {
-	createBooking,
-	updateStatusBooking,
-	deleteDateFromRecurringBooking,
+  createBooking,
+  updateStatusBooking,
+  deleteDateFromRecurringBooking,
 } = bookingsManagmentSlice.actions;
 
 export default bookingsManagmentSlice.reducer;
